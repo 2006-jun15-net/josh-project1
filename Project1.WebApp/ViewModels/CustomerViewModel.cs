5822047a716adf5897733cbec958107e8cfefd1e
Josh Bertrand <joshuabertrand@protonmail.com>
2020-07-14 01:32:42 -0400

    Added Data Annotations to ViewModels

--- Project1.WebApp/ViewModels/CustomerViewModel.cs
+++ Project1.WebApp/ViewModels/CustomerViewModel.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
+﻿using System.ComponentModel.DataAnnotations;
+
 
 namespace Project1.WebApp.ViewModels
 {
--- Project1.WebApp/ViewModels/OrderViewModel.cs
+++ Project1.WebApp/ViewModels/OrderViewModel.cs
@@ -2,22 +2,25 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace Project1.WebApp.ViewModels
 {
     public class OrderViewModel
     {
-        
+        [Display(Name = "Order ID")]
         public int OrderId { get; set; }
+        [Display(Name = "Order Date")]
         public DateTime OrderDate { get; set; }
         [Required]
+        [Display(Name = "Store ID")]
         [Range(1, 999)]
         public int StoreId { get; set; }
         [Required]
+        [Display(Name = "Customer ID")]
         [Range(1, 999)]
         public int CustomerId { get; set; }
+        [Display(Name = "Orders")]
         public Dictionary<ProductEntity, int> Orders { get; set; }
     }
 }
